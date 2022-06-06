224192e5677a461dfe23a7d1cf448ad838ff78c5
AutocorrectGuy <martins.sturainis.lspa@gmail.com>
2022-06-06 09:37:15 +0300

    vesrion 001

--- client/src/components/Partials/navbar/IconsBar.jsx
+++ client/src/components/Partials/navbar/IconsBar.jsx
@@ -1,12 +1,22 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFacebookF, faGithub, faDiscord, faYoutube } from '@fortawesome/free-brands-svg-icons'
+import { faFacebookF, faFacebookMessenger, faGithub, faDiscord, faYoutube } from '@fortawesome/free-brands-svg-icons'
 
 export default function IconsBar({styleClass, parentStyleClass}) {
   return(
     <div className={parentStyleClass}>
-    <FontAwesomeIcon icon={faFacebookF} className={`cursor-pointer ${styleClass}`} />
-    <FontAwesomeIcon icon={faDiscord} className={`cursor-pointer ${styleClass}`} />
-    <FontAwesomeIcon icon={faYoutube} className={`cursor-pointer ${styleClass}`} />
+      <a href={"https://www.facebook.com/profile.php?id=100078427363056"} target="_blank" rel="noreferrer">
+        <FontAwesomeIcon icon={faFacebookMessenger} className={`cursor-pointer ${styleClass}`} />
+      </a>
+      <a href={"https://www.facebook.com/sturainis"} target="_blank" rel="noreferrer">
+        <FontAwesomeIcon icon={faFacebookF} className={`cursor-pointer ${styleClass}`} />
+      </a>
+      {/* <a href={"https://www.facebook.com/sturainis"} target="_blank" rel="noreferrer"> */}
+        <FontAwesomeIcon icon={faDiscord} className={`cursor-pointer ${styleClass}`} />
+      {/* </a> */}
+      <a href={"https://www.youtube.com/channel/UC_KHyjfFuyJM0BDuVIGLH-Q/videos"} target="_blank" rel="noreferrer">
+        <FontAwesomeIcon icon={faYoutube} className={`cursor-pointer ${styleClass}`} />
+      </a>
+      
     <FontAwesomeIcon icon={faGithub} className={`cursor-pointer ${styleClass}`} />
   </div>
   )
